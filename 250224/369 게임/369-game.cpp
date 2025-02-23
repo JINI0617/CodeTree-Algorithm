a538c8b21f4b2b14947af1c2fd55a2ce9b8cfb73
JINI0617 <165622495+JINI0617@users.noreply.github.com>
2025-02-24 00:43:02 +0900

    Title: 369 게임, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 250224/369 게임/369-game.cpp
+++ 250224/369 게임/369-game.cpp
@@ -7,16 +7,22 @@ int main(){
 
     for(int i = 1; i <= N; i++)
     {
-        if(i <= 10)
-        {
+        if(i >= 10)
+            if(i % 3 == 0)
+                cout << 0 << " ";
+            else if(i % 10 == 0)
+                cout << i << " ";
+            else if((i / 10) % 3 == 0)
+                cout << 0 << " ";
+            else if((i % 10) % 3 == 0)
+                cout << 0 << " ";
+            else
+                cout << i << " ";
+        else
             if(i % 3 == 0)
                 cout << 0 << " ";
             else
                 cout << i << " ";
-        }
-        else if((i % 10) % 3 == 0 || (i / 10) % 3 == 0 || i % 3 == 0)
-            cout << 0 << " ";
-        else
-            cout << i << " ";
+            
     }
 }
