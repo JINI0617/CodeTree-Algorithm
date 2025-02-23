5aec8e9e8885c797a3d15525490341b055a20088
JINI0617 <165622495+JINI0617@users.noreply.github.com>
2025-02-23 21:27:36 +0900

    Title: A/B 출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 250223/A/B 출력/a-divide-b.cpp
+++ 250223/A/B 출력/a-divide-b.cpp
@@ -3,23 +3,6 @@ using namespace std;
 
 int main() {
     int A, B;
-#include <iostream>
-using namespace std;
-
-int main() {
-    int A, B;
-    cin >> A >> B;
-    
-    cout << A/B << ".";
-
-    for(int i = 0; i < 20; i++)
-
-
-닫기
-
-0 정답
-
-0 오답
     cin >> A >> B;
     
     cout << A/B << ".";
