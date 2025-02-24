ab2a75d81a3c6e055fa7512118abedb4c251c2b9
JINI0617 <165622495+JINI0617@users.noreply.github.com>
2025-02-25 00:39:58 +0900

    Title: 1까지 나누기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 250225/1까지 나누기/divide-by-1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N, cnt = 0;
+    cin >> N;
+
+    for(int i = 1; i++)
+    {
+        N /= i;
+        cnt++;
+
+        if(N <= 1)
+        {
+            cout << cnt;
+            break;
+        }
+    }
+    return 0;
+}
