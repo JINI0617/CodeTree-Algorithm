a19bbedc85f1c269d83c48aaf2a815686e321582
JINI0617 <165622495+JINI0617@users.noreply.github.com>
2025-02-28 12:34:48 +0900

    Title: 모두 3의 배수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250228/모두 3의 배수/all-multiples-of-3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    bool satisfied = true;
+
+    for(int i = 0; i < 5; i++)
+    {
+        cin >> n;
+
+        if(n % 3 != 0)
+            satisfied = false;
+    }
+
+    if(satisfied == true)
+        cout << 1;
+    else
+        cout << 0;
+
+
+    return 0;
+}
