0d772332881dda95e84bacc2e10798e7db4b7113
JINI0617 <165622495+JINI0617@users.noreply.github.com>
2025-03-08 17:53:24 +0900

    Title: 별표 출력하기 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250308/별표 출력하기 3/print-star-3.cpp
+++ 250308/별표 출력하기 3/print-star-3.cpp
@@ -10,7 +10,7 @@ int main() {
         for(int j = 0; j < i; j++)
             cout << "  ";
         for(int j = 0; j < 2*N - 2*i - 1; j++)
-            cout << "* "
+            cout << "* ";
         cout << endl;
     }
     return 0;
