853d47a7322d410fcb561be54124a628fbefd4e4
JINI0617 <165622495+JINI0617@users.noreply.github.com>
2025-03-10 00:49:44 +0900

    Title: 특정 규칙에 따른 문자 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250310/특정 규칙에 따른 문자 출력/character-output-according-to-rule.cpp
+++ 250310/특정 규칙에 따른 문자 출력/character-output-according-to-rule.cpp
@@ -16,7 +16,7 @@ int main() {
 
     for(int i = 0; i < N-1; i++)
     {
-        for(int j = 0; j < N-i; j++)
+        for(int j = 0; j < N-i-1; j++)
             cout << "@ ";
         cout << endl;
     }
