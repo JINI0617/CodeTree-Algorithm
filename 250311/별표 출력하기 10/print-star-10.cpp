554fc96c55a09bb6955f66814784ff680c9ea6c5
JINI0617 <165622495+JINI0617@users.noreply.github.com>
2025-03-11 01:37:44 +0900

    Title: 별표 출력하기 10, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250311/별표 출력하기 10/print-star-10.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    for(int i = 1; i <= 2*N; i++)
+    {
+        if(i % 2 == 1)
+        {
+            for(int j = 0; j < i/2+1; j++)
+                cout << "* ";
+        }
+        else
+        {
+            for(int j = 0; j < N+1-i/2; j++)
+                cout << "* ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
