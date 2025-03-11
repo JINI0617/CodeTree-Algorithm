89baa289cef5d0079733cee321bc5f482fdbd16b
JINI0617 <165622495+JINI0617@users.noreply.github.com>
2025-03-11 14:34:16 +0900

    Title: 별표 출력하기 12, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250311/별표 출력하기 12/print-star-12.cpp
+++ 250311/별표 출력하기 12/print-star-12.cpp
@@ -5,35 +5,16 @@ int main() {
     int n;
     cin >> n;
 
-    if(n % 2 == 0)
+    for(int i = 1; i <= n; i++)
     {
-        for(int i = 1; i <= n; i++)
+        for(int j = 1; j <= n; j++)
         {
-            for(int j = 1; j <= n; j++)
-            {
-                if(i == 1 || j % 2 == 0 && i <= j)
-                    cout << "* ";
-                else
-                    cout << "  ";
-            }
-            cout << endl;
+            if(i == 1 || j % 2 == 0 && i <= j)
+                cout << "* ";
+            else
+                cout << "  ";
         }
-    }
-    else if(n == 1)
-        cout << "*";
-    else
-    {
-        for(int i = 1; i <= n-1; i++)
-        {
-            for(int j = 1; j <= n; j++)
-            {
-                if(i == 1 || j % 2 == 0 && i <= j)
-                    cout << "* ";
-                else   
-                    cout << "  ";
-            }
-            cout << endl;
-        }
+        cout << endl;
     }
     return 0;
 }
