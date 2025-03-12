0f34dadf69971cb4d032e011544e3f33831b5bcf
JINI0617 <165622495+JINI0617@users.noreply.github.com>
2025-03-12 21:54:44 +0900

    Title: 행과 열, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 250312/행과 열/row-column.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A, B;
+    cin << A << B;
+
+    for(int i = 0; i < A; i++)
+    {
+        for(int j = 0; j < B; j++)
+        {
+            cout << i*j << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
