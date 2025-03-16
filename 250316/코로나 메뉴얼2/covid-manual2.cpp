e248399d89d50b9df44587535c893ada8a831318
JINI0617 <165622495+JINI0617@users.noreply.github.com>
2025-03-16 17:05:29 +0900

    Title: 코로나 메뉴얼2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 250316/코로나 메뉴얼2/covid-manual2.cpp
+++ 250316/코로나 메뉴얼2/covid-manual2.cpp
@@ -2,30 +2,37 @@
 using namespace std;
 
 int main() {
-    int cnt_arr[4] = {0}; // A, B, C, D의 개수
+
+    int cnt_arr[4] = {};
     char s;
     int t;
 
-    for (int i = 0; i < 3; i++) {
+    for(int i = 0; i < 3; i++)
+    {
         cin >> s >> t;
 
-        if (s == 'Y' && t >= 37)
-            cnt_arr[0]++;  // A
-        else if (s == 'N' && t >= 37)
-            cnt_arr[1]++;  // B
-        else if (s == 'Y' && t < 37)
-            cnt_arr[2]++;  // C
+        if(s == 'Y')
+        {
+            if(t >= 37)
+                cnt_arr[0]++;
+            else
+                cnt_arr[1]++;
+        }
         else
-            cnt_arr[3]++;  // D
+        {
+            if(t >= 37)
+                cnt_arr[2]++;
+            else   
+                cnt_arr[3]++;
+        }
     }
 
-    // 결과 출력
-    for (int i = 0; i < 4; i++)
+    for(int i = 0; i < 4; i++)
         cout << cnt_arr[i] << " ";
-
-    // A 카테고리에 해당하는 사람이 2명 이상이면 'E' 출력
-    if (cnt_arr[0] >= 2)
-        cout << "E";
+    
+    if(cnt_arr[0] >= 2)
+        cout << 'E';
+    
 
     return 0;
 }
