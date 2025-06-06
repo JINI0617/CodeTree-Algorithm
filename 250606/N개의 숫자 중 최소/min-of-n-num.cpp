982f29d985c0bec58124e394b542d444f8ba9f02
JINI0617 <165622495+JINI0617@users.noreply.github.com>
2025-06-06 15:08:38 +0900

    Title: N개의 숫자 중 최소, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250606/N개의 숫자 중 최소/min-of-n-num.cpp
+++ 250606/N개의 숫자 중 최소/min-of-n-num.cpp
@@ -5,14 +5,14 @@ using namespace std;
 
 int main() {
     
-    int N, min_val = 100, min_cnt = 0;
+    int N, min_val, min_cnt = 0;
     cin >> N;
     int arr[N];
 
     for(int i = 0; i < N; i++)
-    {
         cin >> arr[i];
-    }
+    
+    min_val = arr[0];
 
     for(int i = 0; i < N; i++)
     {
